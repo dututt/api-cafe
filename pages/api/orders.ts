fca5c42e4cd0c06bb38f8ff37bcdb71b7fb53f97
dutu <du.tu@hitachivantara.com>
2024-09-18 14:04:47 +0700

    update

--- pages/api/orders.ts
+++ pages/api/orders.ts
@@ -4,7 +4,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
-        const result = await pool.query('SELECT o.id, o.table_num, o.price, o.created_at, COUNT(*) as count_items FROM orders as o INNER JOIN order_items as oi ON o.id = oi.order_id GROUP BY o.id, o.table_num');
+        const result = await pool.query('SELECT o.id, o.table_num, o.price, o.status, o.created_at, COUNT(*) as count_items FROM orders as o INNER JOIN order_items as oi ON o.id = oi.order_id GROUP BY o.id, o.table_num');
         res.status(200).json(result.rows);
     } else {
         res.setHeader('Allow', ['GET']);
