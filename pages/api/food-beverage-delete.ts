fcd370b847897024f539d53f93a6638780986432
dutu <du.tu@hitachivantara.com>
2024-09-22 14:12:00 +0700

    update

--- pages/api/food-beverage-delete.ts
+++ pages/api/food-beverage-delete.ts
@@ -6,14 +6,21 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const client = await pool.connect();
     if (req.method === 'DELETE') {
-        await client.query('BEGIN')
+        try {
+            await client.query('BEGIN')
 
-        const updateItemText = `DELETE FROM price USING item WHERE price.item_id=item.id AND item.id =$1 RETURNING *`
-        const updateItemValues = [id]
-        await client.query(updateItemText, updateItemValues)
+            const updateItemText = `DELETE FROM price USING item WHERE price.item_id=item.id AND item.id =$1 RETURNING *`
+            const updateItemValues = [id]
+            await client.query(updateItemText, updateItemValues)
 
-        const result = await client.query('COMMIT')
-        res.status(200).json({ result })
+            const result = await client.query('COMMIT')
+            res.status(200).json({ result })
+        } catch (error) {
+            await client.query('ROLLBACK')
+            res.status(500).json({ error: 'failed to delete item food-beverage' })
+        } finally {
+            client.release()
+        }
     } else {
         res.setHeader('Allow', ['DELETE']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
