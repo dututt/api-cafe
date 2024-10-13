87fd63662908784b84b4cd648bbc32da2f57e8d4
dutu <du.tu@hitachivantara.com>
2024-10-13 18:28:11 +0700

    update

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,12 +1,15 @@
-import pool from '@/lib/db';
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'GET') {
-        const result = await pool.query('SELECT * FROM users');
-        res.status(200).json(result.rows);
-    } else {
-        res.setHeader('Allow', ['GET']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
-    }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    // const result = await pool.query('SELECT * FROM users');
+    // res.status(200).json(result.rows);
+    res.status(200).json([]);
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
